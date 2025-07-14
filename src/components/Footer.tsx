4fe2edb68828c05a21be24878af4ace8217c9783
Song Chaeeun <qlqldkdk0@gmail.com>
2025-07-14 17:10:18 +0900

    ✅ Api bruno 추가

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import FooterHeader from './FooterHeader';
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="bg-poten-red ">
+        <FooterHeader />
+      </footer>
+    </>
+  );
+}
