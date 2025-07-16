43dc4ea6cd8e2cc68a88d10da8e0fe4008521aef
Song Chaeeun <qlqldkdk0@gmail.com>
2025-07-16 18:57:32 +0900

    🎉 Md pick 컴포넌트

--- src/components/MainMdPick/MainMdPick.tsx
+++ src/components/MainMdPick/MainMdPick.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image';
-
 import logo from '../../../public/logo/logo2.svg';
+import { MdItems } from './MdPickItemData';
+import MainMdPickCard from './MainMdPickCard';
 
 export default function MainMdPick() {
+  const ItemList = MdItems.map((item) => (
+    <MainMdPickCard cardItem={item} key={item._id} />
+  ));
   return (
     <>
-      <section className="w-full xl:max-w-[1200px] mx-auto px-[16px] md:px-[24px]  my-[50px]">
+      <section className="w-full xl:max-w-[1200px] mx-auto px-[16px] md:px-[24px] my-[50px]">
         <Image
           className="w-[160px] md:w-[170px]"
           src={logo}
@@ -13,9 +17,12 @@ export default function MainMdPick() {
           height={64}
           alt="포텐게임"
         />
-        <h2 className="text-poten-black font-extrabold text-[26px] md:text-[36px] mt-[4px]">
+        <h2 className="text-poten-black font-extrabold text-[26px] md:text-[36px] mt-[4px] mb-[30px]">
           MD’s PICK 베스트 게임
         </h2>
+        <div className="grid gap-y-5 md:grid-cols-2 md:gap-4 xl:gap-6  auto-rows-auto">
+          {ItemList}
+        </div>
       </section>
     </>
   );
---
+++ src/components/MainMdPick/MainMdPickCard.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import { MdItem } from './MdPickItemData';
+import Link from 'next/link';
+
+export default function MainMdPickCard({ cardItem }: { cardItem: MdItem }) {
+  return (
+    <>
+      <Link href="/">
+        <Image
+          className="rounded-t-[8px]"
+          src={cardItem.src}
+          alt={cardItem.title}
+          width={1920}
+          height={1080}
+        />
+        <div
+          className="bg-poten-snowgray1 p-[20px] rounded-b-[8px] md:h-[270px] flex flex-col
+        justify-around gap-6  xl:p-[36px] leading-tight border-poten-gray-1 border-[1px]">
+          <div>
+            <p className="text-poten-gray-2 font-bold">{cardItem.platform}</p>
+            <h3 className="text-poten-black truncate text-[20px] md:text-[28px] font-extrabold my-2">
+              {cardItem.title}
+            </h3>
+            <p className="text-poten-gray3 leading-normal">{cardItem.contents}</p>
+          </div>
+          <div className="flex flex-col tracking-tighter">
+            <p className="sr-only">가격정보</p>
+            <p className="text-right text-[18px] md:text-[24px] text-poten-gray-2">
+              <span className="sr-only">정상가</span>
+              <s>{cardItem.originalPrice.toLocaleString()}원</s>
+            </p>
+            <p className="text-right font-extrabold text-poten-red text-[28px] md:text-[36px]">
+              <span className="sr-only">최종 할인가</span>
+              {cardItem.price.toLocaleString()}원
+            </p>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+}
