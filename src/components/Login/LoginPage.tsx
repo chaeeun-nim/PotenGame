77af9d96ee6ef8035ae2dbba5c0d2ba1894973bd
dooollee <dooollee@naver.com>
2025-07-22 09:33:27 +0900

    💄 style/로그인 창 간격조정#37

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -28,9 +28,9 @@ export default function LoginPage(){
     <Image className="hidden xl:inline-block w-50 h-20 mt-20 mb-10" src={logo1} alt="메인 로고"/> 
 
     {/* 흰색 박스 */}
-    <div className="bg-white p-10 flex flex-col items-center w-150 ">
+    <div className="bg-white p-10 flex flex-col items-center w-150 mb-30">
 
-      <h1 className="font-bold text-xl mt-10"> 로그인 </h1>
+      <h1 className="font-bold mt-10 text-2xl"> 로그인 </h1>
       <p className="mb-10">포텐게임에 오신것을 환영합니다.</p>
 
       <form action="/" className="w-full flex flex-col">
@@ -57,12 +57,11 @@ export default function LoginPage(){
 
         <LoginButton setIsSubmit={setIsSubmit} type={'submit'} value={'로그인'} color={'red'} />
         <LoginButton link={'/login/signUp'} type={'button'} value={'회원가입'} color={'gray3'} />
-
       </form>
 
 
       <div className="w-full">
-        <h2 className="font-bold text-center my-10">소셜 로그인</h2>
+        <h2 className="font-bold text-center my-10 text-2xl">소셜 로그인</h2>
         <LoginButton type={'button'} value={'카카오 로그인'} color={'kakao'}/>
         <LoginButton type={'button'} value={'네이버 로그인'} color={'naver'}/>
       </div>
