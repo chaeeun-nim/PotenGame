005c14650094384a682e0a64e0a4ce98064a5606
dooollee <dooollee@naver.com>
2025-07-29 17:44:38 +0900

    ✨ feat/이메일, 별명 중복 확인 버튼 #68

--- src/components/SignUp/SignUpInput.tsx
+++ src/components/SignUp/SignUpInput.tsx
@@ -1,3 +1,7 @@
+import { emailCheck } from "@/data/functions/emailCheck";
+import { NameCheck } from "@/data/functions/nicknameCheck";
+import { useState } from "react";
+
 interface SignUpInputType{
   type: string;
   title:string;
@@ -6,6 +10,9 @@ interface SignUpInputType{
 }
 
 export default function SignUpInput({title, type, placeholder, button}: SignUpInputType){
+
+  const [ email, setEmail ] = useState('');
+  const [ name, setName ] = useState('');
 
   const authButton = () => {
     switch(type){
@@ -19,7 +26,26 @@ export default function SignUpInput({title, type, placeholder, button}: SignUpIn
     }
   }
 
-  // const emailRegex = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
+  // 이메일 중복 체크
+  const checkEmail = async (email: string) => {
+    const res = await emailCheck(email);
+    if(res.ok){
+      console.log('가능한 이메일입니다.');
+    }else{
+      console.log('중복된 이메일입니다.');
+    }
+  }
+
+  // 별명 중복 체크
+  const checkText = async (name: string) => {
+    const res = await NameCheck(name);
+    if(res.ok){
+      console.log('가능한 별명입니다.');
+    }else{
+      console.log('중복된 별명입니다.');
+    }
+  }
+
 
   return(
       <div className="mb-6 h-20 ">
@@ -35,12 +61,20 @@ export default function SignUpInput({title, type, placeholder, button}: SignUpIn
             id={type} 
             type={type} 
             placeholder={placeholder} 
+            value={type === "email"? email: type === "text"? name : undefined}
+            onChange={(e) => type === "email" ? setEmail(e.target.value) : type === "text"? setName(e.target.value): undefined}
             className="block bg-poten-inputgray rounded-lg p-3 w-full
                         md:basis-4/5 flex-grow"
           />
 
           {button &&
             <button 
+              type="button"
+              onClick={ type === "email" 
+                        ? () => checkEmail(email)
+                        : type === "text" 
+                        ? () => checkText(name)
+                        :  undefined}
               className="float-right my-2 md:my-0 bg-poten-gray3 text-white font-bold whitespace-nowrap p-3 border-none rounded-lg ml-6 
                           md:basis-1/5">
               { authButton() }
