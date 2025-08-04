d3e276207a672396e091b2e50b6dc27dbcb814d3
Song Chaeeun <qlqldkdk0@gmail.com>
2025-08-04 13:08:31 +0900

    💄 찜하기 여백수정

--- src/app/myPage/like/page.tsx
+++ src/app/myPage/like/page.tsx
@@ -1,9 +1,12 @@
 import LikepageHeader from '@/components/LikePage/LikepageHeader';
 import LikePageList from '@/components/LikePage/LikePageList';
+import MobileMyPageNav from '@/components/MyPage/MobileMyPageNav';
 
 export default function LikePage() {
   return (
     <>
+      {/* 모바일 전용 버튼 그룹 */}
+      <MobileMyPageNav />
       <LikepageHeader />
       <div className="grid grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-16">
         <LikePageList />
--- src/components/LikePage/LikepageHeader.tsx
+++ src/components/LikePage/LikepageHeader.tsx
@@ -8,7 +8,7 @@ export default function LikepageHeader() {
   return (
     <>
       {Like.length === 0 ? (
-        <header className="  w-full gap-4 xl:max-w-[1200px]  xl:mx-auto  my-[18px] md:my-[32px]">
+        <header className="  w-full gap-4 xl:max-w-[1200px]  xl:mx-auto  mb-[18px] md:mb-[32px]">
           <div className="flex gap-4 border-b-2 border-poten-gray-1 pb-[14px] md:pb-[20px] justify-start items-center">
             <span className="block w-[5px] h-[20px] md:h-[24px] rounded-[2px] bg-poten-red"></span>
             <h1 className="font-bold text-[18px] xl:text-[22px]">
@@ -17,7 +17,7 @@ export default function LikepageHeader() {
           </div>
         </header>
       ) : (
-        <header className="  w-full gap-4 xl:max-w-[1200px]  xl:mx-auto  my-[18px] md:my-[32px]">
+        <header className="  w-full gap-4 xl:max-w-[1200px]  xl:mx-auto  mb-[18px] md:mb-[32px]">
           <div className="flex gap-4 border-b-2 border-poten-gray-1 pb-[14px] md:pb-[20px] justify-start items-center">
             <span className="block w-[5px] h-[20px] md:h-[24px] rounded-[2px] bg-poten-red"></span>
             <h1 className="font-bold text-[18px] xl:text-[22px]">
