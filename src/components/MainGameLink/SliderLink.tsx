ff567a75c0be3a750ecbc4ec33acfe123c6f6245
twkimfe <arthurkim135@naver.com>
2025-08-05 12:03:09 +0900

    ✨ feat: 메인 페이지 SliderLink 컴포넌트 카테고리별 페이지 이동 기능 구현 - /components/MainGameLink/SliderLink.tsx     - 게임기별 카테고리 페이지 경로 매핑 추가 (Nintendo Switch → /list/NINTENDO01 등)     - GameConsole 인터페이스와 배열 구조로 데이터 체계화 및 동적 렌더링 구현

--- src/components/MainGameLink/SliderLink.tsx
+++ src/components/MainGameLink/SliderLink.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import linkGame2 from '@/assets/img/01-swtich.webp';
@@ -9,7 +9,53 @@ import linkGame4 from '@/assets/img/01-ps4.webp';
 import linkGame5 from '@/assets/img/01-ps5.webp';
 import Link from 'next/link';
 
+// 게임기별 카테고리 정보 타입 정의
+interface GameConsole {
+  id: string;
+  name: string;
+  displayName: string;
+  image: StaticImageData;
+  alt: string;
+  categoryPath: string;
+}
+
 export default function SliderLink() {
+  // 게임기별 카테고리 정보 배열
+  const gameConsoles: GameConsole[] = [
+    {
+      id: 'nintendo-switch',
+      name: 'Nintendo Switch',
+      displayName: 'Nintendo Switch',
+      image: linkGame2,
+      alt: '닌텐도 스위치',
+      categoryPath: '/list/NINTENDO01',
+    },
+    {
+      id: 'nintendo-switch-2',
+      name: 'Nintendo Switch 2',
+      displayName: 'Nintendo Switch 2',
+      image: linkGame3,
+      alt: '닌텐도 스위치 2',
+      categoryPath: '/list/NINTENDO02',
+    },
+    {
+      id: 'playstation-4',
+      name: 'PS 4',
+      displayName: 'PS 4',
+      image: linkGame4,
+      alt: '플레이 스테이션4',
+      categoryPath: '/list/PLAYSTATION04',
+    },
+    {
+      id: 'playstation-5',
+      name: 'PS 5',
+      displayName: 'PS 5',
+      image: linkGame5,
+      alt: '플레이 스테이션 5',
+      categoryPath: '/list/PLAYSTATION05',
+    },
+  ];
+
   return (
     <>
       <Swiper
@@ -27,62 +73,26 @@ export default function SliderLink() {
         freeMode={true}
         navigation={false}
         slidesPerView={'auto'}>
-        <SwiperSlide className="!w-[90px] md:!w-[130px] xl:!w-[150px] !ml-[16px] md:!ml-[12px] xl:!ml-[0px]  ">
-          <Link
-            href="/"
-            className="flex flex-col justify-center items-center transition-transform ease-in-out hover:translate-y-[-10px]">
-            <Image
-              className="md:rounded"
-              src={linkGame2}
-              width={800}
-              height={800}
-              alt="닌텐도 스위치"
-            />
-            <h3 className="text-[14px] text-center">Nintendo Switch</h3>
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide className="!w-[90px] md:!w-[130px] xl:!w-[150px] !ml-[16px] md:!ml-[12px] xl:!ml-[0px]  ">
-          <Link
-            href="/"
-            className="flex flex-col justify-center items-center transition-transform ease-in-out hover:translate-y-[-10px]">
-            <Image
-              className="md:rounded"
-              src={linkGame3}
-              width={800}
-              height={800}
-              alt="닌텐도 스위치 2"
-            />
-            <h3 className="text-[14px] text-center">Nintendo Switch 2</h3>
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide className="!w-[90px] md:!w-[130px] xl:!w-[150px] !ml-[16px] md:!ml-[12px] xl:!ml-[0px]  ">
-          <Link
-            href="/"
-            className="flex flex-col justify-center items-center transition-transform ease-in-out hover:translate-y-[-10px]">
-            <Image
-              className="md:rounded"
-              src={linkGame4}
-              width={800}
-              height={800}
-              alt="플레이 스테이션4"
-            />
-            <h3 className="text-[14px] text-center">PS 4</h3>
-          </Link>
-        </SwiperSlide>
-        <SwiperSlide className="!w-[90px] md:!w-[130px] xl:!w-[150px] !ml-[16px] md:!ml-[12px] xl:!ml-[0px]  md:!mr-[24px]">
-          <Link
-            href="/"
-            className="flex flex-col justify-center items-center transition-transform ease-in-out hover:translate-y-[-10px]">
-            <Image
-              className="md:rounded"
-              src={linkGame5}
-              width={800}
-              height={800}
-              alt="플레이 스테이션 5"
-            />
-            <h3 className="text-[14px] text-center">PS 5</h3>
-          </Link>
-        </SwiperSlide>
+        {gameConsoles.map((console, index) => (
+          <SwiperSlide
+            key={console.id}
+            className={`!w-[90px] md:!w-[130px] xl:!w-[150px] !ml-[16px] md:!ml-[12px] xl:!ml-[0px] ${
+              index === gameConsoles.length - 1 ? 'md:!mr-[24px]' : ''
+            }`}>
+            <Link
+              href={console.categoryPath}
+              className="flex flex-col justify-center items-center transition-transform ease-in-out hover:translate-y-[-10px]">
+              <Image
+                className="md:rounded"
+                src={console.image}
+                width={800}
+                height={800}
+                alt={console.alt}
+              />
+              <h3 className="text-[14px] text-center">{console.displayName}</h3>
+            </Link>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
