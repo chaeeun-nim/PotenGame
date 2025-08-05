c070022257eb22aa3f0de2e7aa6a5e9b85c93a3a
dooollee <dooollee@naver.com>
2025-08-05 18:04:49 +0900

    ✨ feat/회원수정 페이지 중복체크#111

--- src/app/myPage/profile/page.tsx
+++ src/app/myPage/profile/page.tsx
@@ -12,82 +12,18 @@ export default function ProfilePage() {
       <section className="hidden xl:block bg-white mb-10">
         {/* 제목 */}
         <div className="flex items-center border-b border-gray-200 pb-2 mb-6">
-          <div className="w-[4px] h-[20px] bg-[var(--color-poten-red)] mr-2 rounded-sm" />
-          <h2 className="text-lg font-bold text-[var(--color-poten-red)]">기본정보 변경</h2>
-
-          <ProfileInput name={"name"} title={"별명"} type={"text"} placeholder={"별명을 입력해주세요"}/>
-          <ProfileInput name={"email"} title={"이메일"} type={"email"} placeholder={"이메일을 입력해주세요"}/>
-          <ProfileInput name={"phone"} title={"휴대폰 번호"} type={"phone"} placeholder={"전화번호를 입력해주세요"}/>
+          <div className="w-[4px] h-[20px] bg-poten-red mr-2 rounded-sm" />
+          <h2 className="text-lg font-bold text-poten-red">기본정보 변경</h2>
         </div>
 
         {/* 별명 */}
-        <div className="mb-4">
-          <label className="text-sm font-semibold mb-1 block">
-            별명<span className="text-red-500">*</span>
-          </label>
-          <div className="flex gap-4">
-            <input
-              type="text"
-              value="물범비린내"
-              className="flex-1 h-[50px] bg-[#f1f4f6] px-4 rounded text-sm text-black"
-              readOnly
-            />
-            <button className="w-[180px] h-[50px] bg-[var(--color-poten-gray3)] text-white rounded">
-              별명 변경
-            </button>
-          </div>
-        </div>
+        <ProfileInput name={"nickname"} title={"별명"} type={"text"}/>
 
         {/* 이메일 */}
-        <div className="mb-4">
-          <label className="text-sm font-semibold mb-1 block">
-            이메일<span className="text-red-500">*</span>
-          </label>
-          <div className="flex gap-4">
-            <input
-              type="email"
-              value="sealsmell@naver.com"
-              className="flex-1 h-[50px] bg-[#f1f4f6] px-4 rounded text-sm text-black"
-              readOnly
-            />
-            <button className="w-[180px] h-[50px] bg-[var(--color-poten-gray3)] text-white rounded">
-              이메일 변경
-            </button>
-          </div>
-        </div>
-
-        {/* 인증 완료 */}
-        <div className="flex items-center gap-2 mb-4 ml-1">
-          <div className="w-4 h-4 rounded-full bg-green-500 flex items-center justify-center text-[10px] text-white">
-            ✔
-          </div>
-          <span className="text-sm text-green-600 font-medium">인증완료</span>
-        </div>
+        <ProfileInput name={"email"} title={"이메일"} type={"email"}/>
 
         {/* 휴대폰 번호 */}
-        <div className="mb-2">
-          <label className="text-sm font-semibold mb-1 block">
-            휴대폰 번호<span className="text-red-500">*</span>
-          </label>
-          <div className="flex gap-4">
-            <input
-              type="tel"
-              placeholder="휴대폰 번호를 입력해 주세요."
-              className="flex-1 h-[50px] bg-[#f1f4f6] px-4 rounded text-sm placeholder-[var(--color-poten-gray-2)]"
-            />
-            <button className="w-[180px] h-[50px] bg-[var(--color-poten-gray3)] text-white rounded">
-              휴대폰 인증
-            </button>
-          </div>
-        </div>
-
-        {/* 인증대기 표시 */}
-        <div className="flex items-center gap-2 mt-2 ml-1">
-          <div className="w-4 h-4 rounded-full bg-orange-400 flex items-center justify-center text-white text-[10px] font-bold">
-            !
-          </div>
-          <span className="text-sm text-orange-500 font-medium">인증대기</span>
-        </div>
+        <ProfileInput name={"phone"} title={"휴대폰 번호"} type={"phone"}/>
 
         {/* 하단 버튼 */}
         <div className="flex justify-end gap-3 mt-8">
--- src/components/MyPage/Profile/ProfileInput.tsx
+++ src/components/MyPage/Profile/ProfileInput.tsx
@@ -1,167 +1,74 @@
 import { emailCheck } from "@/data/functions/emailCheck";
 import { NicknameCheck } from "@/data/functions/nicknameCheck";
+import { useState } from "react";
 
-interface SignUpInputType {
-  type: string;
+
+interface ProfileInputType {
   title: string;
-  placeholder: string;
-  name:string;
-
-  email?: string;
-  nickname?: string;
-  tel?: string;
-  emailMessage?: string;
-  nicknameMessage?: string;
-
-  setEmail?: (value: string) => void;
-  setEmailMessage?: (value: string) => void;
-  setNickname?: (value: string) => void;
-  setNicknameMessage?: (value: string) => void;
-  setTel?:  (value: string) => void;
+  type: string
+  name: string;
 }
 
-export default function ProfileInput({
-  title,
-  type,
-  placeholder,
-  name,
-  
+export default function ProfileInput({title, type, name}: ProfileInputType) {
 
-  email,
-  nickname,
-  tel,
-  emailMessage,
-  nicknameMessage,
-  setEmail,
-  setEmailMessage,
-  setNickname,
-  setNicknameMessage,
-  setTel
-}: SignUpInputType) {
+  const [ value, setValue ] = useState('');
+  const [ canUse, setCanUse ] = useState(false);
 
-  const emailRegex =
-    /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
-
-  // 버튼 텍스트
-  const authButton = () => {
-    switch (title) {
-      case "이메일":
-      case "별명":
-        return "중복 확인";
-      case "휴대폰 번호":
-        return "휴대폰 인증";
-      default:
-        return null;
-    }
-  };
-
-  // 이메일 중복체크
-  const checkEmail = async () => {
-    if (!email || !setEmailMessage) return;
-    const res = await emailCheck(email);
-
-    if (!emailRegex.test(email)) {
-      setEmailMessage("아이디는 이메일 형식으로 입력해주세요");
-    } else {
-      setEmailMessage(res.ok ? "사용가능" : "이미 존재하는 이메일입니다.");
-    }
-  };
-
-  // 별명 중복체크
-  const checkText = async () => {
-    if (!nickname || !setNicknameMessage) return;
-    const res = await NicknameCheck(nickname);
-    setNicknameMessage(res.ok ? "사용가능" : "이미 존재하는 별명입니다.");
-  };
-
-  // input value 처리
-  const getValue = () => {
-    switch (title) {
-      case "이메일":
-        return email;
-      case "별명":
-        return nickname;
-      case "휴대폰 번호":
-        return tel;
-      default:
-        return "";
-    }
-  };
-
-  const handleChange = (value: string) => {
-    switch (title) {
-      case "이메일":
-        setEmail?.(value);
+  const DupCheck = async () =>{
+    switch(name){
+      case "nickname":
+        const nickname = await NicknameCheck(value);
+        if(nickname.ok) setCanUse(true)
         break;
-      case "별명":
-        setNickname?.(value);
+      case "email":
+        const email = await emailCheck(value);
+        if(email.ok) setCanUse(true)
         break;
-      case "휴대폰 번호":
-        setTel?.(value);
-        break;
-      default:
-        break;
+      default: return null;
     }
-  };
+  }
 
   return (
-    <div className="mb-6 h-20">
-      <label htmlFor={title}>
-        {title}
-        <span className="text-poten-red">*</span>
-      </label>
+    <div>
+      {/* 인풋창 */}
+        <div className="mb-4">
+          <label className="text-sm font-semibold mb-1 block">
+            {title}<span className="text-red-500">*</span>
+          </label>
+          <div className="flex gap-4">
+            <input
+              type={type}
+              value={value}
+              onChange={ e => setValue(e.target.value)}
+              className="flex-1 h-[50px] bg-[#f1f4f6] px-4 rounded text-sm text-black"
+            />
+            <button 
+              className="w-[180px] h-[50px] bg-poten-gray3 text-white rounded"
+              onClick={() => DupCheck()}
+              >
+              { name === 'phone'? "휴대폰 인증" : "중복확인"}
+            </button>
+          </div>
+        </div>
 
-      <div className="md:flex justify-between">
-        <input
-          name={name}
-          id={title}
-          type={type}
-          placeholder={placeholder}
-          value={getValue()}
-          onChange={(e) => handleChange(e.target.value)}
-          className="block bg-poten-inputgray rounded-lg p-3 w-full md:basis-4/5 flex-grow"
-        />
-
-        <button
-          type="button"
-          onClick={
-            title === "이메일"
-              ? checkEmail
-              : title === "별명"
-              ? checkText
-              : undefined
+        {/* 확인 메세지 */}
+        <div className="flex items-center gap-2 mb-4 ml-1">
+          { canUse ? 
+          <>
+            <div className="w-4 h-4 rounded-full bg-green-500 flex items-center justify-center text-[10px] text-white">
+              ✔
+            </div>
+            <span className="text-sm text-green-600 font-medium">사용가능</span>
+          </>
+          :
+          <>
+            <div className="w-4 h-4 rounded-full bg-orange-400 flex items-center justify-center text-white text-[10px] font-bold">
+              !
+            </div>
+            <span className="text-sm text-orange-500 font-medium">사용불가</span>
+          </>
           }
-          className="float-right my-2 md:my-0 bg-poten-gray3 text-white font-bold whitespace-nowrap p-3 border-none rounded-lg ml-6 md:basis-1/5"
-        >
-          {authButton()}
-        </button>
-
-      </div>
-
-      {/* 메시지 출력 */}
-      {title === "이메일" && emailMessage && (
-        <p
-          className={`font-bold ${
-            emailMessage === "사용가능"
-              ? "text-poten-safegreen"
-              : "text-poten-error-color"
-          }`}
-        >
-          {emailMessage}
-        </p>
-      )}
-
-      {title === "별명" && nicknameMessage && (
-        <p
-          className={`font-bold ${
-            nicknameMessage === "사용가능"
-              ? "text-poten-safegreen"
-              : "text-poten-error-color"
-          }`}
-        >
-          {nicknameMessage}
-        </p>
-      )}
+        </div>
     </div>
   );
 }
